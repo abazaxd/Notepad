9509fa5cdc19200b002f612b433a7ed440006f53
Vlad <abaza-vlad@mail.ru>
2022-05-06 14:19:08 +0300

    Готовая версия без базы данных

--- app/src/main/java/com/example/notepad/presentation/PageActivity.kt
+++ app/src/main/java/com/example/notepad/presentation/PageActivity.kt
@@ -6,15 +6,76 @@ import android.graphics.pdf.PdfDocument
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
 import com.example.notepad.R
+import com.example.notepad.domain.Page
 
 class PageActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: PageViewModel
+
+    private lateinit var etText: EditText
+    private lateinit var btSave: Button
+
+    private var screenMode = MODE_UNKNOWN
+    private var pageId = Page.DEFAULT_ID
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_edit_layout)
+        parseIntent()
+        viewModel = ViewModelProvider(this)[PageViewModel::class.java]
+        initViews()
+        when (screenMode) {
+            MODE_EDIT -> launchEditMode()
+            MODE_ADD -> launchADDMode()
+        }
 
+        viewModel.finishActivity.observe(this){
+            finish()
+        }
+
+    }
+
+    private fun launchEditMode(){
+        viewModel.getPage(pageId)
+        viewModel.page.observe(this) {
+            etText.setText(it.text)
+        }
+        btSave.setOnClickListener {
+            viewModel.editPage(etText.text?.toString())
+        }
+    }
+
+    private fun launchADDMode() {
+        btSave.setOnClickListener {
+            viewModel.addPage(etText.text?.toString())
+        }
+    }
+
+    private fun parseIntent(){
+        if (!intent.hasExtra(EXTRA_SCREEN_MODE)){
+            throw RuntimeException("Param screen mode is absent.")
+        }
         val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
-        Log.d("PageActivity", mode.toString())
+        if (mode != MODE_EDIT && mode != MODE_ADD){
+            throw RuntimeException("Unknown screen mode.")
+        }
+        screenMode = mode
+        if (screenMode == MODE_EDIT){
+            if (!intent.hasExtra(EXTRA_PAGE_ID)) {
+                    throw RuntimeException("Param Page Id is absent.")
+                }
+            pageId = intent.getIntExtra(EXTRA_PAGE_ID, Page.DEFAULT_ID)
+        }
+    }
+
+    private fun initViews(){
+
+        etText = findViewById(R.id.etText)
+        btSave = findViewById(R.id.btSave)
     }
     companion object {
 
@@ -22,6 +83,7 @@ class PageActivity : AppCompatActivity() {
         private const val EXTRA_PAGE_ID = "extra_page_id"
         private const val MODE_EDIT = "mode_edit"
         private const val MODE_ADD = "mode_add"
+        private const val MODE_UNKNOWN = ""
 
         fun newIntentAddPage(context: Context): Intent{
             val intent = Intent(context, PageActivity::class.java)
